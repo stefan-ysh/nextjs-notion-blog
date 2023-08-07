4421bb5094cce2201ac3d880b01ce6de537a263d
yshuai <stefan_ysh@foxmail.com>
2023-08-07 18:32:21 +0800

    chore(style): 更改主题切换按钮位置，从底部改为顶部

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -36,9 +36,9 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2022 {config.author}</div>
+      <div className={styles.copyright}>Copyright 2023 {config.author}</div>
 
-      <div className={styles.settings}>
+      {/* <div className={styles.settings}>
         {hasMounted && (
           <a
             className={styles.toggleDarkMode}
@@ -50,7 +50,7 @@ export const FooterImpl: React.FC = () => {
             {isDarkMode ? <IoMoonSharp /> : <IoSunnyOutline />}
           </a>
         )}
-      </div>
+      </div> */}
 
       <div className={styles.social}>
         {config.twitter && (
