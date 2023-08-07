70b6afc3772171d780c970f626b03291dba726d3
yshuai <stefan_ysh@foxmail.com>
2023-08-07 17:16:09 +0800

    fix: hide page side content

--- components/PageAside.tsx
+++ components/PageAside.tsx
@@ -7,24 +7,32 @@ import { getPageTweet } from '@/lib/get-page-tweet'
 import { PageActions } from './PageActions'
 import { PageSocial } from './PageSocial'
 
+// export const PageAside: React.FC<{
+//   block: Block
+//   recordMap: ExtendedRecordMap
+//   isBlogPost: boolean
+// }> = ({ block, recordMap, isBlogPost }) => {
+//   if (!block) {
+//     return null
+//   }
+
+//   // only display comments and page actions on blog post pages
+//   if (isBlogPost) {
+//     const tweet = getPageTweet(block, recordMap)
+//     if (!tweet) {
+//       return null
+//     }
+
+//     return <PageActions tweet={tweet} />
+//   }
+
+//   return <PageSocial />
+// }
+
 export const PageAside: React.FC<{
   block: Block
   recordMap: ExtendedRecordMap
   isBlogPost: boolean
 }> = ({ block, recordMap, isBlogPost }) => {
-  if (!block) {
-    return null
-  }
-
-  // only display comments and page actions on blog post pages
-  if (isBlogPost) {
-    const tweet = getPageTweet(block, recordMap)
-    if (!tweet) {
-      return null
-    }
-
-    return <PageActions tweet={tweet} />
-  }
-
-  return <PageSocial />
+  return null
 }
