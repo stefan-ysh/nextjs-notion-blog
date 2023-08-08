2d0c8cc66c28a7f698db2e7be044459493de7e2f
yshuai <stefan_ysh@foxmail.com>
2023-08-08 14:26:05 +0800

    chore(style): 调整 loading icon 的样式

--- components/LoadingIcon.tsx
+++ components/LoadingIcon.tsx
@@ -12,52 +12,105 @@ export const LoadingIcon = (props) => {
       {...rest}
       viewBox='0 0 24 24'
     >
-      <defs>
-        <linearGradient
-          x1='28.1542969%'
-          y1='63.7402344%'
-          x2='74.6289062%'
-          y2='17.7832031%'
-          id='linearGradient-1'
-        >
-          <stop stopColor='rgba(164, 164, 164, 1)' offset='0%' />
-          <stop
-            stopColor='rgba(164, 164, 164, 0)'
-            stopOpacity='0'
-            offset='100%'
-          />
-        </linearGradient>
-      </defs>
-
-      <g id='Page-1' stroke='none' strokeWidth='1' fill='none'>
-        <g transform='translate(-236.000000, -286.000000)'>
-          <g transform='translate(238.000000, 286.000000)'>
-            <circle
-              id='Oval-2'
-              stroke='url(#linearGradient-1)'
-              strokeWidth='4'
-              cx='10'
-              cy='12'
-              r='10'
-            />
-            <path
-              d='M10,2 C4.4771525,2 0,6.4771525 0,12'
-              id='Oval-2'
-              stroke='rgba(164, 164, 164, 1)'
-              strokeWidth='4'
-            />
-            <rect
-              id='Rectangle-1'
-              fill='rgba(164, 164, 164, 1)'
-              x='8'
-              y='0'
-              width='4'
-              height='4'
-              rx='8'
-            />
-          </g>
-        </g>
-      </g>
+      <rect
+        x='0'
+        y='7.6416'
+        width='4'
+        height='14.7168'
+        fill='#00aeff9b'
+        opacity='0.2'
+      >
+        <animate
+          attributeName='opacity'
+          attributeType='XML'
+          values='0.2; 1; .2'
+          begin='0s'
+          dur='0.6s'
+          repeatCount='indefinite'
+        ></animate>
+        <animate
+          attributeName='height'
+          attributeType='XML'
+          values='10; 20; 10'
+          begin='0s'
+          dur='0.6s'
+          repeatCount='indefinite'
+        ></animate>
+        <animate
+          attributeName='y'
+          attributeType='XML'
+          values='10; 5; 10'
+          begin='0s'
+          dur='0.6s'
+          repeatCount='indefinite'
+        ></animate>
+      </rect>
+      <rect
+        x='8'
+        y='5.1416'
+        width='4'
+        height='19.7168'
+        fill='#00aeff9b'
+        opacity='0.2'
+      >
+        <animate
+          attributeName='opacity'
+          attributeType='XML'
+          values='0.2; 1; .2'
+          begin='0.15s'
+          dur='0.6s'
+          repeatCount='indefinite'
+        ></animate>
+        <animate
+          attributeName='height'
+          attributeType='XML'
+          values='10; 20; 10'
+          begin='0.15s'
+          dur='0.6s'
+          repeatCount='indefinite'
+        ></animate>
+        <animate
+          attributeName='y'
+          attributeType='XML'
+          values='10; 5; 10'
+          begin='0.15s'
+          dur='0.6s'
+          repeatCount='indefinite'
+        ></animate>
+      </rect>
+      <rect
+        x='16'
+        y='7.3584'
+        width='4'
+        height='15.2832'
+        fill='#00aeff9b'
+        opacity='0.2'
+      >
+        <animate
+          attributeName='opacity'
+          attributeType='XML'
+          values='0.2; 1; .2'
+          begin='0.3s'
+          dur='0.6s'
+          repeatCount='indefinite'
+        ></animate>
+        <animate
+          attributeName='height'
+          attributeType='XML'
+          values='10; 20; 10'
+          begin='0.3s'
+          dur='0.6s'
+          repeatCount='indefinite'
+        ></animate>
+        <animate
+          attributeName='y'
+          attributeType='XML'
+          values='10; 5; 10'
+          begin='0.3s'
+          dur='0.6s'
+          repeatCount='indefinite'
+        ></animate>
+      </rect>
     </svg>
   )
 }
